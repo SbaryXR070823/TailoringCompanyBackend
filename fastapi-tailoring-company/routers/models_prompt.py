6a0f25bc810bb3b208dc8713aea6d41e2616af39
Ionut Alexandru Sbaroi <sbaroialex@gmail.com>
2025-01-11 16:25:17 +0200

    Added back promp for material ML model, deleted it by mistake

--- fastapi-tailoring-company/routers/models_prompt.py
+++ fastapi-tailoring-company/routers/models_prompt.py
@@ -55,6 +55,17 @@ def deserialize_model(stored_data):
     
     return model, encoder
 
+@router.post("/prompt_materials_predictions/{material_id}")
+async def load_model_materials_price_predictions(material_id: str):
+    stored_model = await mongodb_service.find_one(
+        collection_name='model_storage',
+        query={"model_name": "material_price_model", "isLatest" : True}
+    )
+    
+    model = pickle.loads(stored_model['model'])
+    
+    return await predict_next_year_price(model, mongodb_service, material_id)
+
 @router.post("/prompt_workmanship_prediction")
 async def predict_product_workmanship(request: dict):
     try:
